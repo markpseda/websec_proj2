55249f17d2c7f502657b17cadd64a3ddd6ea1ec6
Mark Seda <markpseda@gmail.com>
2018-11-20 18:40:18 -0500

    Working with OAuth, example code

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,23 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createsmatch = functions.database.ref("/people/{personid}").onWrite(
+function(thechange, context){
+    var personid = context.params.personid;
+    var data = thechange.after.val();
+
+    if (!data){
+        return null;
+    }
+    
+    var actionsRef = thechange.after.ref.parent.parent.child("actioncount");
+    return actionsRef.transaction(function(counter){
+        if (!!counter || counter === 0){
+            counter += 1;
+        } else {
+            counter = 1;
+        }
+        return counter;
+    });
+});
